5333aacbc08352094bb246f0482b595ecc70d4bf
flight9 <ealeaf@sohu.com>
2017-08-14 17:51:22 +0800

    1. button style on link 2. show thumbnail 3. multi fileds have their own Id

--- js/wechat_image_fun.js
+++ js/wechat_image_fun.js
@@ -3,10 +3,6 @@
 //    alert('config ok.');
 //});
 
-//wx.error(function(res){
-//    alert('failed');
-//});
-
 jQuery( function() {
     wx.config({
         debug: false,                    	//true开启调试模式。
@@ -15,31 +11,46 @@ jQuery( function() {
         nonceStr: Drupal.settings.tri_wechat_image_field.nonceStr, 
         signature: Drupal.settings.tri_wechat_image_field.signature,
         jsApiList: ['chooseImage','uploadImage','scanQRCode'],	//看具体要调用的接口
-    });    
+    });
+    
+    wx.error(function(res){
+        alert('Error:wx.config failed!');
+    });
 });
 
-function takePhoto() {
+function takePhoto(alink) {
+    var textfield = jQuery(alink).parent().find('input.textfield-of-serverid');
+    var thumbnail = jQuery(alink).parent().find('img.thumbnail-of-serverid');
+    //console.log(thumbnail.attr('src'));
+    
+    if( textfield.val().length != 0) {
+        alert('not empty!');
+    }
+    
     wx.chooseImage({
         count: 1, // 默认9
         sizeType: ['original', 'compressed'], //'original', 'compressed'
-        sourceType: ['camera'], 			// 'album', 'camera'
+        sourceType: ['camera'],             // 'album', 'camera'
         success: function (res) {
-            var localIds = res.localIds; // localId可以作为img标签的src属性显示图片
-            uploadPhoto(localIds[0]);
+            var localId = res.localIds[0];      // localId可以作为img标签的src属性显示图片
+            uploadPhoto( localId);
         }
     });
+    
+    function uploadPhoto( localId) {
+        wx.uploadImage({
+            localId: localId, 
+            isShowProgressTips: 1, // 显示进度提示
+            success: function (res) {
+                var serverId = res.serverId; 
+                textfield.val(serverId);
+                thumbnail.attr('src', localId);
+            }
+        });
+    }
 }
 
-function uploadPhoto( localId) {
-    wx.uploadImage({
-        localId: localId, 
-        isShowProgressTips: 1, // 显示进度提示
-        success: function (res) {
-            var serverId = res.serverId; 
-            jQuery('input.textfield-of-serverid').val(serverId);
-        }
-    });
-}
+
 
 function scanUrl() {
     wx.scanQRCode({
